a0ec47d84b6a785c222b1744f61873b704fc6a5a
samaa-2002 <132456121+samaa-2002@users.noreply.github.com>
2023-11-10 19:04:07 +0200

    Add files via upload

---
+++ task_1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      int sum = 0;
+      // getting the sum of numbers from 1 to 5
+      for (int i=1;i<=5;i++){
+          sum+=i;
+
+      }
+        System.out.println(sum);
+    }
+}
